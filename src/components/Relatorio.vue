917970ebd9e4013bbdcfea9832aa3170a27eb6c4
thainawatanabe <48865153+thainawatanabe@users.noreply.github.com>
2019-07-15 01:09:05 -0300

    src/components/Relatorio.vue

---
+++ src/components/Relatorio.vue
@@ -0,0 +1,55 @@
+<template>
+<!-- próximo passo seria utilizar os dados dos endereços e transformá-los em dados para serem usados no gráfico -->
+  <div class="small">
+    <line-chart :chart-data="datacollection"></line-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<!!-- próximo passo seria utilizar os dados dos endereços e transformá-los em dados para serem usados no gráfico -->
+
+<script>
+  import LineChart from './LineChart.js'
+
+  export default {
+    components: {
+      LineChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    max-width: 600px;
+    margin:  150px auto;
+  }
+</style>
