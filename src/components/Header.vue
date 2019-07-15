4e171af2df571987862b9056c8c15ae80b4421b7
thainawatanabe <48865153+thainawatanabe@users.noreply.github.com>
2019-07-15 01:07:42 -0300

    src/components/Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+<!-- próximo passo seria criar uma array para armazenar os dados! Em seguida, dar funcionalidade ao botão "Editar",
+onde seria possível editar o nome do estabelecimento e buscar o endereço. -->
+    <div>
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Hi Technologies</h5>
+                    <p class="card-text">R. Eduardo Sprada, 6400B</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Universidade Positivo</h5>
+                    <p class="card-text">R. Prof. Pedro Viriato Parigot de Souza, 5300</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Google</h5>
+                    <p class="card-text">Av. Brg. Faria Lima, 3477</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Incorp Technology</h5>
+                    <p class="card-text">R. Djalma Farias, 241</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">TecVitoria</h5>
+                    <p class="card-text">R. Marins Alvarino, 150</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="https://i.imgur.com/8FkDa4P.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Nubank</h5>
+                    <p class="card-text">R. Capote Valente, 39</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <b-button href="#" variant="primary">Editar</b-button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script scoped>
+    
+</script>
