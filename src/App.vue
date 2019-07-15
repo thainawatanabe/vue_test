5339b5c60c362584d04c87a00a959fdd687a58fe
thainawatanabe <48865153+thainawatanabe@users.noreply.github.com>
2019-07-15 01:27:59 -0300

    changes in the app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header />
+        <QuestionBox />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Header from './components/Header.vue'
+    import QuestionBox from './components/Relatorio.vue'
+    import CommitChart from './components/LineChart.js'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+      Header,
+      QuestionBox
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import InstantSearch from 'vue-instantsearch';
+import VueCharts from 'vue-chartjs'
+import { Bar, Line } from 'vue-chartjs'
+
 import App from './App.vue'
+
+Vue.use(InstantSearch);
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
